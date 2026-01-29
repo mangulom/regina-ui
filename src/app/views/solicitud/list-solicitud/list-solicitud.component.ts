57b0a7e1df29794d03bf521f2d155491e0b34ce5
mangulom <mauricioangulo4@gmail.com>
2026-01-29 11:45:59 -0500

    Commit Correcto

--- src/app/views/solicitud/list-solicitud/list-solicitud.component.ts
+++ src/app/views/solicitud/list-solicitud/list-solicitud.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-solicitud',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './list-solicitud.component.html',
   styleUrl: './list-solicitud.component.scss'
 })
